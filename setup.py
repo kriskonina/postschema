964b901ed7c763788b6dd1539467cf1d45e92bf5
Kris Kavalieri <kris.kavalieri@gmail.com>
2019-08-25 20:13:41 +0200

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='postschema',
+    version='0.5.0',
+    description='Async python ORM for postgres',
+    url='git@github.com:kriskavalieri/postschema.git',
+    author='Kris Kavalieri',
+    author_email='admin@bookven.com',
+    license='unlicense',
+    packages=['postschema'],
+    zip_safe=False
+)
