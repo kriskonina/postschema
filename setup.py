ab60a911a4c79951be23b59bae9bd44e334d8082
Kris Kavalieri <kris.kavalieri@gmail.com>
2019-10-02 19:28:13 +0200

    update postman link

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='postschema',
-    version='0.6.0',
+    version='0.6.1',
     description='Async python ORM for postgres',
     url='https://github.com/kriskavalieri/postschema',
     author='Kris Kavalieri',
