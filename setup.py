c0232d6fd84b82e355b569df3a7f91e93faa00d7
Kris Kavalieri <kris.kavalieri@gmail.com>
2019-12-23 02:02:51 +0100

    bump to 0.9.27

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='postschema',
-    version='0.9.26',
+    version='0.9.27',
     description='Async python ORM for postgres',
     url='https://github.com/kriskavalieri/postschema',
     author='Kris Kavalieri',
