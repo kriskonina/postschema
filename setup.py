362a86d6933839fae1c976fbcbec635ad9be47e7
Kris Kavalieri <kris.kavalieri@gmail.com>
2020-03-07 17:03:24 +0100

    Bump to 0.13.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='postschema',
-    version='0.13.2',
+    version='0.13.3',
     description='Async python ORM for postgres',
     url='https://github.com/kriskavalieri/postschema',
     author='Kris Kavalieri',
